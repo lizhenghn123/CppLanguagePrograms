b7d5fec58f4b2e9e86fa6fcfcff88183a5156d78
lizhenghn123 <lizhenghn@gmail.com>
2014-04-30 17:48:26 +0800

    add macro for different os platforms
    Signed-off-by: lizhenghn123 <lizhenghn@gmail.com>

--- include/osDefine.h
+++ include/osDefine.h
@@ -2,9 +2,21 @@
 #define OSDEFINE_FILE_H
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
-#define OS_WINDOWS				/**Windows*/	
+# define OS_WINDOWS
+#elif defined(__CYGWIN__) || defined(__CYGWIN32__)
+# define OS_CYGWIN
+#elif defined(linux) || defined(__linux) || defined(__linux__)
+# define OS_LINUX
+#elif defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__)
+# define OS_MACOSX
+#elif defined(__FreeBSD__)
+# define OS_FREEBSD
+#elif defined(__NetBSD__)
+# define OS_NETBSD
+#elif defined(__OpenBSD__)
+# define OS_OPENBSD
 #else
-#define OS_LINUX				/**Linux*/
+#error	"You Must Be Choose One Platform"
 #endif
 
 #endif
