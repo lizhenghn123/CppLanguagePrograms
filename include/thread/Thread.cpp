18981b7b291c2963f44085b4f9d9895c98728f9b
lizhenghn123 <lizhenghn@gmail.com>
2014-09-04 18:26:04 +0800

    add thread class(come from http://tinythreadpp.bitsnbites.eu/); add BitHacks(come from http://graphics.stanford.edu/~seander/bithacks.html);

---
+++ include/thread/Thread.cpp
@@ -0,0 +1,162 @@
+#include "Thread.h"
+#if defined(OS_WINDOWS)
+#include <process.h>
+#else
+#include <unistd.h>
+#endif
+
+NAMESPACE_ZL_START
+
+/// Information to pass to the new thread (what to run).
+struct _thread_start_info 
+{
+	void (*mFunction)(void *); ///< Pointer to the function to be executed.
+	void * mArg;               ///< Function argument for the thread function.
+	thread * mThread;          ///< Pointer to the thread object.
+};
+
+// Thread wrapper function.
+#if defined(OS_WINDOWS)
+unsigned WINAPI thread::wrapper_function(void * aArg)
+#else
+void * thread::wrapper_function(void * aArg)
+#endif
+{
+	// Get thread startup information
+	_thread_start_info * ti = (_thread_start_info *) aArg;
+
+	try
+	{
+		// Call the actual client thread function
+		ti->mFunction(ti->mArg);
+	}
+	catch(...)
+	{
+		// Uncaught exceptions will terminate the application (default behavior
+		// according to C++11)
+		std::terminate();
+	}
+
+	// The thread is no longer executing
+	MutexGuard guard(ti->mThread->mDataMutex);
+	ti->mThread->mNotAThread = true;
+
+	// The thread is responsible for freeing the startup information
+	delete ti;
+
+	return 0;
+}
+
+thread::thread(void (*aFunction)(void *), void * aArg)
+{
+	// Serialize access to this thread structure
+	MutexGuard guard(mDataMutex);
+
+	// Fill out the thread startup information (passed to the thread wrapper,
+	// which will eventually free it)
+	_thread_start_info * ti = new _thread_start_info;
+	ti->mFunction = aFunction;
+	ti->mArg = aArg;
+	ti->mThread = this;
+
+	// The thread is now alive
+	mNotAThread = false;
+
+	// Create the thread
+#if defined(OS_WINDOWS)
+	mHandle = (HANDLE) _beginthreadex(0, 0, wrapper_function, (void *) ti, 0, &mWin32ThreadID);
+#elif defined(_TTHREAD_POSIX_)
+	if(pthread_create(&mHandle, NULL, wrapper_function, (void *) ti) != 0)
+		mHandle = 0;
+#endif
+
+	// Did we fail to create the thread?
+	if(!mHandle)
+	{
+		mNotAThread = true;
+		delete ti;
+	}
+}
+
+thread::~thread()
+{
+	if(joinable())
+		std::terminate();
+}
+
+void thread::join()
+{
+	if(joinable())
+	{
+#if defined(OS_WINDOWS)
+		WaitForSingleObject(mHandle, INFINITE);
+		CloseHandle(mHandle);
+#elif defined(_TTHREAD_POSIX_)
+		pthread_join(mHandle, NULL);
+#endif
+	}
+}
+
+bool thread::joinable() const
+{
+	MutexGuard lock(mDataMutex);
+	bool result = !mNotAThread;
+	return result;
+}
+
+void thread::detach()
+{
+	MutexGuard lock(mDataMutex);
+	if(!mNotAThread)
+	{
+#if defined(OS_WINDOWS)
+		CloseHandle(mHandle);
+#elif defined(_TTHREAD_POSIX_)
+		pthread_detach(mHandle);
+#endif
+		mNotAThread = true;
+	}
+}
+
+thread::id thread::get_id() const
+{
+	if(!joinable())
+		return id();
+#if defined(OS_WINDOWS)
+	return id((unsigned long int) mWin32ThreadID);
+#elif defined(_TTHREAD_POSIX_)
+	return _pthread_t_to_ID(mHandle);
+#endif
+}
+
+unsigned thread::hardware_concurrency()
+{
+#if defined(OS_WINDOWS)
+	SYSTEM_INFO si;
+	GetSystemInfo(&si);
+	return (int) si.dwNumberOfProcessors;
+#elif defined(_SC_NPROCESSORS_ONLN)
+	return (int) sysconf(_SC_NPROCESSORS_ONLN);
+#elif defined(_SC_NPROC_ONLN)
+	return (int) sysconf(_SC_NPROC_ONLN);
+#else
+	// The standard requires this function to return zero if the number of
+	// hardware cores could not be determined.
+	return 0;
+#endif
+}
+
+
+//------------------------------------------------------------------------------
+// this_thread
+//------------------------------------------------------------------------------
+thread::id this_thread::get_id()
+{
+#if defined(OS_WINDOWS)
+	return thread::id((unsigned long int) GetCurrentThreadId());
+#elif defined(_TTHREAD_POSIX_)
+	return _pthread_t_to_ID(pthread_self());
+#endif
+}
+
+NAMESPACE_ZL_END
---
+++ thread/Test_Thread/Test_Thread.cpp
@@ -0,0 +1,267 @@
+#include <iostream>
+#include "thread/Thread.h"
+#include "thread/Mutex.h"
+#include "thread/Condition.h"
+using namespace std;
+using namespace ZL;
+
+
+
+// Thread local storage variable
+#ifndef NO_TLS
+thread_local int gLocalVar;
+#endif
+
+// Mutex + global count variable
+Mutex gMutex;
+int gCount;
+
+// Condition variable
+Condition gCond(gMutex);
+
+// Thread function: Thread ID
+void ThreadIDs(void * aArg)
+{
+	cout << " My thread id is " << this_thread::get_id() << "." << endl;
+}
+
+#ifndef NO_TLS
+// Thread function: Thread-local storage
+void ThreadTLS(void * aArg)
+{
+	gLocalVar = 2;
+	cout << " My gLocalVar is " << gLocalVar << "." << endl;
+}
+#endif
+
+// Thread function: Mutex locking
+void ThreadLock(void * aArg)
+{
+	for(int i = 0; i < 10000; ++ i)
+	{
+		MutexGuard lock(gMutex);
+		++ gCount;
+	}
+}
+
+// Thread function: Condition notifier
+void ThreadCondition1(void * aArg)
+{
+	MutexGuard lock(gMutex);
+	-- gCount;
+	gCond.NotifyAll();
+}
+
+// Thread function: Condition waiter
+void ThreadCondition2(void * aArg)
+{
+	cout << " Wating..." << flush;
+	MutexGuard lock(gMutex);
+	while(gCount > 0)
+	{
+		cout << "." << flush;
+		gCond.Wait();
+	}
+	cout << "." << endl;
+}
+
+// Thread function: Yield
+void ThreadYield(void * aArg)
+{
+	// Yield...
+	this_thread::yield();
+}
+
+// Thread function: Detach
+void ThreadDetach(void * aArg)
+{
+	// We don't do anything much, just sleep a little...
+	this_thread::sleep_for(chrono::milliseconds(100));
+}
+
+void test_thread()
+{
+	// Test 1: Show number of CPU cores in the system
+	cout << "PART I: Info" << endl;
+	cout << " Number of processor cores: " << thread::hardware_concurrency() << endl;
+
+	// Test 2: thread IDs
+	cout << endl << "PART II: Thread IDs" << endl;
+	{
+		// Show the main thread ID
+		cout << " Main thread id is " << this_thread::get_id() << "." << endl;
+
+		// Start a bunch of child threads - only run a single thread at a time
+		thread t1(ThreadIDs, 0);
+		t1.join();
+		thread t2(ThreadIDs, 0);
+		t2.join();
+		thread t3(ThreadIDs, 0);
+		t3.join();
+		thread t4(ThreadIDs, 0);
+		t4.join();
+	}
+
+	// Test 3: thread local storage
+	cout << endl << "PART III: Thread local storage" << endl;
+#ifndef NO_TLS
+	{
+		// Clear the TLS variable (it should keep this value after all threads are
+		// finished).
+		gLocalVar = 1;
+		cout << " Main gLocalVar is " << gLocalVar << "." << endl;
+
+		// Start a child thread that modifies gLocalVar
+		thread t1(ThreadTLS, 0);
+		t1.join();
+
+		// Check if the TLS variable has changed
+		if (gLocalVar == 1)
+			cout << " Main gLocalID was not changed by the child thread - OK!" << endl;
+		else
+			cout << " Main gLocalID was changed by the child thread - FAIL!" << endl;
+	}
+#else
+	cout << " TLS is not supported on this platform..." << endl;
+#endif
+
+	// Test 4: mutex locking
+	cout << endl << "PART IV: Mutex locking (100 threads x 10000 iterations)" << endl;
+	{
+		// Clear the global counter.
+		gCount = 0;
+
+		// Start a bunch of child threads
+		list<thread *> threadList;
+		for (int i = 0; i < 100; ++i)
+			threadList.push_back(new thread(ThreadLock, 0));
+
+		// Wait for the threads to finish
+		list<thread *>::iterator it;
+		for (it = threadList.begin(); it != threadList.end(); ++it)
+		{
+			thread * t = *it;
+			t->join();
+			delete t;
+		}
+
+		// Check the global count
+		cout << " gCount = " << gCount << endl;
+	}
+
+	// Test 6: condition variable
+	cout << endl << "PART VI: Condition variable (40 + 1 threads)" << endl;
+	{
+		// Set the global counter to the number of threads to run.
+		gCount = 40;
+
+		// Start the waiting thread (it will wait for gCount to reach zero).
+		thread t1(ThreadCondition2, 0);
+
+		// Start a bunch of child threads (these will decrease gCount by 1 when they
+		// finish)
+		list<thread *> threadList;
+		for (int i = 0; i < 40; ++i)
+			threadList.push_back(new thread(ThreadCondition1, 0));
+
+		// Wait for the waiting thread to finish
+		t1.join();
+
+		// Wait for the other threads to finish
+		list<thread *>::iterator it;
+		for (it = threadList.begin(); it != threadList.end(); ++it)
+		{
+			thread * t = *it;
+			t->join();
+			delete t;
+		}
+	}
+
+	// Test 7: yield
+	cout << endl << "PART VII: Yield (40 + 1 threads)" << endl;
+	{
+		// Start a bunch of child threads
+		list<thread *> threadList;
+		for (int i = 0; i < 40; ++i)
+			threadList.push_back(new thread(ThreadYield, 0));
+
+		// Yield...
+		this_thread::yield();
+
+		// Wait for the threads to finish
+		list<thread *>::iterator it;
+		for (it = threadList.begin(); it != threadList.end(); ++it)
+		{
+			thread * t = *it;
+			t->join();
+			delete t;
+		}
+	}
+
+	// Test 8: sleep
+	cout << endl << "PART VIII: Sleep (10 x 100 ms)" << endl;
+	{
+		// Sleep...
+		cout << " Sleeping" << flush;
+		for (int i = 0; i < 10; ++i)
+		{
+			this_thread::sleep_for(chrono::milliseconds(100));
+			cout << "." << flush;
+		}
+		cout << endl;
+	}
+
+	// Test 9: detach
+	cout << endl << "PART IX: Detach" << endl;
+	{
+		thread t(ThreadDetach, 0);
+		//t.detach(); 这个有问题, 因为t释放时，其回调函数并没有结束
+		t.join();
+		cout << " Detached from thread." << endl;
+	}
+}
+
+class  TestT
+{
+public:
+	static void print_hello()
+	{
+		std::cout << "hello world\n";
+	}
+
+	void print_num(int i)
+	{
+		std::cout << "num :" << num + i << "\n";
+	}
+
+	int num ;
+};
+
+void exec_testt(void *arg)
+{
+	TestT *pt = (TestT*) arg;
+	TestT::print_hello();
+	pt->print_num(6);
+	this_thread::sleep_for(chrono::milliseconds(1000));
+}
+int main()
+{
+	test_thread();
+	
+	// Test 9: detach,  这个在windows下有问题, 因为t释放时，其回调函数并没有结束
+	cout << endl << "PART IX: Detach" << endl;
+	{
+		thread t(ThreadDetach, 0);
+		t.detach();
+		this_thread::sleep_for(chrono::milliseconds(100));
+		cout << " Detached from thread." << endl;
+	}
+
+	TestT test;
+	test.num = 101;
+	thread t1(exec_testt, &test);
+	t1.join();
+
+	system("pause");
+	return 0;
+}
