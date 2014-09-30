4c524ed19f953e848d83c449da86811ed08dd748
lizhenghn123 <lizhenghn@gmail.com>
2014-10-01 00:36:53 +0800

    增加一个RAII类自动释放资源；

---
+++ tools/ScopeExitGuard/test_ScopeExitGuard.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <Windows.h>
+#include <assert.h>
+#include "base/ScopeExitGuard.hpp"
+using namespace std;
+
+
+template < typename T >
+struct FClose
+{
+	void operator()(T handle)
+	{
+		int ret = fclose(handle);
+		cout << ret << "\n";
+	}
+};
+
+
+int main()
+{
+
+	{
+		FILE *file = fopen("f.log", "w");
+		//ScopeExitGuard<FILE*, FClose, 0> ff(file);
+		//ScopeExitGuard<FILE*, detail::DeletePolicy, 0> ff(file);
+		ScopeCloseFile ff(file);
+	    fwrite("123", 1, 3, file);
+		//auto_file ff(file);
+	}
+	{
+		int *p = new int;
+		//ScopeExitGuard<int*, detail::DeletePolicy, 0> ff(p);
+		ScopeExitGuard<int*> fqf(p);
+		//ScopeDeleteMem ff(p);
+		*p = 5;
+	}
+	return 0;
+}
