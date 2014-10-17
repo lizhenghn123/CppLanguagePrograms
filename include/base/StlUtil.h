abed001d7728d42d6ac392ac8ccb52bf40fd0417
lizhenghn123 <lizhenghn@gmail.com>
2014-10-18 00:13:45 +0800

    add StlUtil.h

--- include/base/StlUtil.h
+++ include/base/StlUtil.h
@@ -15,51 +15,73 @@
 #include <sstream>
 NAMESPACE_ZL_START
 
-// STLDeleteContainerPointers()
-//  For a range within a container of pointers, calls delete
-//  (non-array version) on these pointers.
-// NOTE: for these three functions, we could just implement a DeleteObject
-// functor and then call for_each() on the range and functor, but this
-// requires us to pull in all of algorithm.h, which seems expensive.
-// For hash_[multi]set, it is important that this deletes behind the iterator
-// because the hash_set may call the hash function on the iterator when it is
-// advanced, which could result in the hash function trying to deference a
-// stale pointer.
-template <class ForwardIterator>
-void STLDeleteContainerPointers(ForwardIterator begin,
-                                ForwardIterator end) {
-  while (begin != end) {
-    ForwardIterator temp = begin;
-    ++begin;
-    delete *temp;
+/** 使用一对迭代器，删除每个迭代器所指向的指针 */
+template <typename ForwardIterator>
+void StlDeleteContainerPointer(ForwardIterator begin, ForwardIterator end)
+{
+  while (begin != end) 
+  {       
+      ForwardIterator tmp = begin;
+      ++begin;
+      Safe_Delete(*tmp);
   }
 }
 
-// STLDeleteElements() deletes all the elements in an STL container and clears
-// the container.  This function is suitable for use with a vector, set,
-// hash_set, or any other STL container which defines sensible begin(), end(),
-// and clear() methods.
-//
-// If container is NULL, this function is a no-op.
-//
-// As an alternative to calling STLDeleteElements() directly, consider
-// ElementDeleter (defined below), which ensures that your container's elements
-// are deleted when the ElementDeleter goes out of scope.
+/** 删除某一容器包含的指针 */
 template <class T>
-void STLDeleteElements(T *container) {
-  if (!container) return;
-  STLDeleteContainerPointers(container->begin(), container->end());
+void StlDeleteElems(T& container) {
+    if (container.empty()) 
+        return;
+    StlDeleteContainerPointer(container.begin(), container.end());
+    container.clear();
+}
+
+/** 删除某一容器包含的指针 */
+template <class T>
+void StlDeleteElems(T *container) {
+  if (!container) 
+      return;
+  StlDeleteContainerPointer(container->begin(), container->end());
   container->clear();
 }
 
+/** 删除关联容器中的value值（指针） */
 template <class T>
-void STLDeleteValues(T *v) {
-  if (!v) return;
-  for (typename T::iterator i = v->begin(); i != v->end(); ++i) {
-    delete i->second;
+void StlDeleteValues(T& v) 
+{
+    if (v.empty()) 
+        return;
+    for (typename T::iterator i = v.begin(); i != v.end(); ++i) 
+    {
+        Safe_Delete(i->second);
+    }
+    v.clear();
+}
+
+/** 删除关联容器中的value值（指针） */
+template <class T>
+void StlDeleteValues(T *v) 
+{
+  if (!v) 
+      return;
+  for (typename T::iterator i = v->begin(); i != v->end(); ++i) 
+  {
+    Safe_Delete(i->second);
   }
   v->clear();
 }
+
+/** 仿函数：删除map/multimap/hash_map中的value指针，可用于for_each等算法中 */
+template<typename TKEY, typename TVALUE>
+struct del_map_second_ptr
+{
+public :
+    template<typename TKEY, typename TVALUE>
+    void operator()(std::pair<TKEY, TVALUE *> p)
+    {
+        Safe_Delete(p.second);
+    }
+};
 
 NAMESPACE_ZL_END
 #endif  /* ZL_STLUTIL_H */
