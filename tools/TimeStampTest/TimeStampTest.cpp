cf03a161208542f3bc4655c5a028a235c4c8a3f5
lizhenghn123 <lizhenghn@gmail.com>
2014-10-30 18:28:01 +0800

    update Timestamp

--- tools/TimeStampTest/TimeStampTest.cpp
+++ tools/TimeStampTest/TimeStampTest.cpp
@@ -11,12 +11,12 @@ int main()
     {
         const long long kMicroSecondsPerSecond = 1000 * 1000;
         char buf[32] = {0};
-        time_t seconds = static_cast<time_t>(time1.micoSeconds() / kMicroSecondsPerSecond);
+        time_t seconds = static_cast<time_t>(time1.microSeconds() / kMicroSecondsPerSecond);
         struct tm *tm_time = localtime(&seconds);
          printf("++++%ld\n", seconds);
         if (1)
         {
-            int microseconds = static_cast<int>(time1.micoSeconds() % kMicroSecondsPerSecond);
+            int microseconds = static_cast<int>(time1.microSeconds() % kMicroSecondsPerSecond);
             _snprintf(buf, sizeof(buf), "%4d-%02d-%02d %02d:%02d:%02d:%06d\n",
                 tm_time->tm_year + 1900, tm_time->tm_mon + 1, tm_time->tm_mday,
                 tm_time->tm_hour, tm_time->tm_min, tm_time->tm_sec,
@@ -40,6 +40,16 @@ int main()
                 microseconds);
             printf("%s\n", buf);
         }
+    }
+    {
+        Timestamp now = Timestamp::now();
+        printf("%s\n", now.toString().c_str());
+        Timestamp n3 = now + 3;
+        printf("%s\n", n3.toString().c_str());
+        n3 = 10.01 + n3;
+        printf("%s\n", n3.toString().c_str());
+
+        printf("diff : %f\n", Timestamp::timediff(n3, now));
     }
     system("pause");
 }
