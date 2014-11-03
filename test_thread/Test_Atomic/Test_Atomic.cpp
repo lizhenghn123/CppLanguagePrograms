d6aab482248c6839646bef335ec87134806fdac8
lizhenghn123 <lizhenghn@gmail.com>
2014-11-03 18:26:34 +0800

    修改Atomic为模板类； 增加Atomic关于bool类型的偏特化版本； 注：windows版本不好实现啊，使用mutex的方式，后期可能会删除windows版本

--- test_thread/Test_Atomic/Test_Atomic.cpp
+++ test_thread/Test_Atomic/Test_Atomic.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
 	{
-		zl::thread::Atomic ato;
+		zl::thread::Atomic<int> ato;
 		ato++;
 		++ato;
 		ato.fetchAndInc(10);
@@ -19,7 +19,7 @@ int main()
 	}
 	{
 		std::cout << "========================================\n";
-		zl::thread::Atomic ato;
+		zl::thread::Atomic<int> ato;
 
 		std::cout << ato.fetchAndInc(2) << "\t" ;
 		std::cout << ato.value() << "\n";
@@ -29,11 +29,31 @@ int main()
 	}
 	{
 		std::cout << "========================================\n";
-		zl::thread::Atomic ato;
+		zl::thread::Atomic<int> ato;
 
 		std::cout << ato.incAndFetch(2) << "\t" ;
 		std::cout << ato << "\n";
 	}
+    {
+        std::cout << "========================================\n";
+        zl::thread::Atomic<bool> ato;
+        cout << ato << "\n" ;
+        cout << ato.test_and_set() << "\n";
+        cout << ato << "\n" ;
+
+        ato = true;
+        cout << ato << "\n";
+        ato = false;
+        cout << ato << "\n";
+
+        ato = false;
+        ato = false;
+        cout << ato << "\n";
+
+        ato = true;
+        ato.clear();
+        cout << ato << "\n";
+    }
 	system("pause");
 	return 0;
 }
